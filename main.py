29fba8c441b1a15c6be3946c4744e969379b567a
Marco Junior <marco.junior@celeirosementes.com>
2025-05-19 11:30:03 -0300

    Deploy final com Home funcional

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
-
-# Monta pastas estáticas e de templates
-app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
