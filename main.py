2675dacd644e7c59bc5a74332de8313eb61605ae
Marco Junior <marco.junior@celeirosementes.com>
2025-05-19 11:13:06 -0300

    Deploy final com layout corrigido e home funcional

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-# FastAPI app configurado com rotas e banco de dados SQLite
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+# Monta pastas estáticas e de templates
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
