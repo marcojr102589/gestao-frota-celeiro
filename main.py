a25da8cf0eff13b139b30279d4be41c4e60a9f0e
Marco Junior <marco.junior@celeirosementes.com>
2025-05-19 14:25:38 -0300

    Deploy final funcionando do sistema de gestão de frota

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
-
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
+app.mount('/static', StaticFiles(directory='static'), name='static')
+templates = Jinja2Templates(directory='templates')
 
-@app.get("/", response_class=HTMLResponse)
+@app.get('/', response_class=HTMLResponse)
 async def home(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request})
+    return templates.TemplateResponse('home.html', {'request': request})
 
-@app.get("/reserva", response_class=HTMLResponse)
+@app.get('/reserva', response_class=HTMLResponse)
 async def reserva(request: Request):
-    return templates.TemplateResponse("reserva.html", {"request": request})
+    return templates.TemplateResponse('reserva.html', {'request': request})
 
-@app.get("/devolucao", response_class=HTMLResponse)
+@app.get('/devolucao', response_class=HTMLResponse)
 async def devolucao(request: Request):
-    return templates.TemplateResponse("devolucao.html", {"request": request})
+    return templates.TemplateResponse('devolucao.html', {'request': request})
 
-@app.get("/login", response_class=HTMLResponse)
+@app.get('/login', response_class=HTMLResponse)
 async def login(request: Request):
-    return templates.TemplateResponse("login.html", {"request": request})
+    return templates.TemplateResponse('login.html', {'request': request})
