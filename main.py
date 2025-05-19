350bbb495ba0eea80576d3d2f8d180016c1f01bb
Marco Junior <marco.junior@celeirosementes.com>
2025-05-19 14:07:27 -0300

    Corrige nomes de arquivos e finaliza estrutura de templates

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-import os
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse("home.html", {"request": request})
+
+@app.get("/reserva", response_class=HTMLResponse)
+async def reserva(request: Request):
+    return templates.TemplateResponse("reserva.html", {"request": request})
+
+@app.get("/devolucao", response_class=HTMLResponse)
+async def devolucao(request: Request):
+    return templates.TemplateResponse("devolucao.html", {"request": request})
+
+@app.get("/login", response_class=HTMLResponse)
+async def login(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
